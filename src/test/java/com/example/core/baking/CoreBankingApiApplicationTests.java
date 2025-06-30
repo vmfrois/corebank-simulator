3452ac6cb82d8faaeae18a1fe464ad98dc3de43a
vinicius.frois <viniciusfroism@gmail.com>
2025-06-30 18:41:00 -0300

    init

---
+++ src/test/java/com/example/core/baking/CoreBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.core.baking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
